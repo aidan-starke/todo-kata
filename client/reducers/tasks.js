32aba5ad6c0b04b99ac1b511661000b67bed5111
aidan-starke <starke.aidan@gmail.com>
2020-09-11 08:35:24 +1200

    install redux, provider, global store

---
+++ client/reducers/tasks.js
@@ -0,0 +1,10 @@
+const initialState = [
+    { id: 1, task: 'test' },
+    { id: 2, task: 'pls' }
+]
+
+function viewTasks(state = initialState, action) {
+    return state
+}
+
+export default viewTasks
