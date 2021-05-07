530b521b147f4ba1afc63a704ef066517038e1a8
aidan-starke <starke.aidan@gmail.com>
2021-05-07 20:42:05 +1200

    add cors

--- client/api.js
+++ client/api.js
@@ -1,20 +1,15 @@
 import request from 'superagent'
 
-const url = 'http://localhost:4000/api/v1/tasks'
+const url = 'api/v1/tasks'
 
 export function getList() {
-    return request
-        .get(url)
-        .then(res => res.body)
+	return request.get(url).then(res => res.body)
 }
 
 export function deleteTaskById(id) {
-    return request
-        .delete(`${url}/${id}`)
+	return request.delete(`${url}/${id}`)
 }
 
 export function addNewTask(task) {
-    return request 
-        .post(url)
-        .send({task})
+	return request.post(url).send({ task })
 }
